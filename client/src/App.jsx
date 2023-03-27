e01c14ed374481bf6358100a855dea2b1ac9c219
Evan Astle <starphys@gmail.com>
2023-03-27 15:13:14 -0700

    DB expanded, basic React pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react"
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from "./routes/Home";
+import ProfessorPage from "./routes/ProfessorPage";
+import ReviewPage from "./routes/ReviewPage";
 
 const App = () => {
     return(
-        <div>Hello World!</div>
+        <Router>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/professor/:id' component={ProfessorPage} />
+                <Route exact path='/review' component={ReviewPage} />
+            </Switch>
+        </Router>
     )
 }
 
--- db.sql
+++ db.sql
@@ -1,23 +1,74 @@
-/*
-    -- Variables
-    Unique identifier
-    Full name
-    Overall rating
-    Spider graph stat values
-        Easy
-        Effective/Educational/Learned a lot
-        Fair Grader
-        Would retake/would recommend
-
-
-    -- Needs more thought
-    Schools - tightly coupled with courses
-    Courses - it would be great to lookup professors by course and courses by professor
-    Ratings - should definitely be its own table
-
+/**
+* Untested, but this should represent the fields and relationships we intend to implement.
 */
-CREATE TABLE professors (
+CREATE TABLE school (
     id serial not null primary key,
-    name text not null,
-    rating numeric not null
+    school_name text not null,
+    hide_flag boolean not null default false
+);
+
+CREATE TABLE professor (
+    id serial not null primary key,
+    first_name text not null,
+    last_name text not null,
+    degrees text,
+    overall numeric not null,
+    quality1 numeric not null,
+    quality2 numeric not null,
+    quality3 numeric not null,
+    quality4 numeric not null,
+    quality5 numeric not null,
+    hide_flag boolean not null default false
+);
+
+CREATE TABLE course (
+    id serial not null primary key,
+    course_name text not null,
+    school_id int not null,
+    constraint fk_school FOREIGN KEY(school_id) REFERENCES school(id),
+    hide_flag boolean not null default false
+);
+
+CREATE TABLE student (
+    id serial not null primary key,
+    first_name text not null,
+    last_name text not null,
+    username text not null,
+    p_word text not null,
+    verified boolean not null default false,
+    hide_flag boolean not null default false
+);
+
+CREATE TABLE review (
+    id serial not null primary key,
+    
+    review text,
+    overall numeric not null,
+    quality1 numeric not null,
+    quality2 numeric not null,
+    quality3 numeric not null,
+    quality4 numeric not null,
+    quality5 numeric not null,
+    
+    course_id text not null,
+    school_id int not null,
+    professor_id int not null,
+    student_id int not null,
+    constraint fk_course FOREIGN KEY(course_id) REFERENCES course(id),
+    constraint fk_school FOREIGN KEY(school_id) REFERENCES school(id),
+    constraint fk_professor FOREIGN KEY(professor_id) REFERENCES professor(id),
+    constraint fk_student FOREIGN KEY(student_id) REFERENCES student(id),
+    hide_flag boolean not null default false
+);
+
+CREATE TABLE course_professor (
+    course_id int REFERENCES course(course_id) ON UPDATE CASCADE,
+    professor_id int REFERENCES professor(professor_id) ON UPDATE CASCADE,
+    CONSTRAINT pk_course_professor PRIMARY KEY(course_id, professor_id)
+);
+
+CREATE TABLE school_professor (
+    school_id int REFERENCES school(school_id) ON UPDATE CASCADE,
+    professor_id int REFERENCES professor(professor_id) ON UPDATE CASCADE,
+    CONSTRAINT pk_school_professor PRIMARY KEY(school_id, professor_id)
 );
